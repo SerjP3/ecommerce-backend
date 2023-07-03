228cbf9dfe9c7bdd1710d55a39b928af0851c7dc
sprodan <sprodan@deloittece.com>
2023-07-03 09:46:36 +0300

    Data Objects

--- src/main/java/com/myproject/ecommercebackend/TestDataObject.java
+++
@@ -1,4 +0,0 @@
-package com.myproject.ecommercebackend;
-
-public class TestDataObject{
-}
---
+++ src/main/java/com/myproject/ecommercebackend/model/Address.java
@@ -0,0 +1,77 @@
+package com.myproject.ecommercebackend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "addres_line_1", nullable = false, length = 512)
+    private String addresLine1;
+
+    @Column(name = "address_line_2", length = 512)
+    private String addressLine2;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "country", nullable = false, length = 75)
+    private String country;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private LocalUser user;
+
+    public LocalUser getUser() {
+        return user;
+    }
+
+    public void setUser(LocalUser user) {
+        this.user = user;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getAddresLine1() {
+        return addresLine1;
+    }
+
+    public void setAddresLine1(String addresLine1) {
+        this.addresLine1 = addresLine1;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
